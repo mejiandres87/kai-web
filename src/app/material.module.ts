0f9410c1cf9a406e938eeab5a32bb0084d3c94ba
Andrés Felipe Mejía <mejia.andres87@gmail.com>
2020-09-10 20:18:04 -0500

    fixed missing import

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,9 +3,10 @@ import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
 import { MatCardModule } from "@angular/material/card";
 import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatCardModule, MatDatepickerModule],
-    exports: [MatButtonModule, MatIconModule, MatCardModule, MatDatepickerModule]
+    imports: [MatButtonModule, MatIconModule, MatCardModule, MatDatepickerModule, MatFormFieldModule],
+    exports: [MatButtonModule, MatIconModule, MatCardModule, MatDatepickerModule, MatFormFieldModule]
 })
 export class MaterialModule { }
